e099e6071d6da8e1cf4e6dfd9858fadf1ff5f03c
Stefan Kaestle <stefan.kaestle@dfinity.org>
2022-03-10 13:49:37 +0100

    Lots of extra debug output

--- ic/agent.py
+++ ic/agent.py
@@ -7,7 +7,8 @@ from .constants import *
 from .utils import to_request_id
 from .certificate import lookup
 import hashlib
-
+import re
+from termcolor import colored
 
 def sign_request(req, iden):
     req_id = to_request_id(req)
@@ -27,14 +28,30 @@ def sign_request(req, iden):
         envelop.update({
             'sender_delegation': [iden.delegation],
         })
+
+        # Need to byte-encode various fields
         envelop["sender_delegation"][0]["signature"] = bytes(envelop["sender_delegation"][0]["signature"])
         envelop["sender_delegation"][0]["delegation"]["pubkey"] = \
             bytes(envelop["sender_delegation"][0]["delegation"]["pubkey"])
-        print(colored("Sender public key for delegation " + bytes(iden.delegation_sender_pubkey).hex(), "yellow"))
+        
+        print(colored("Sender public key for delegation " + \
+                      bytes(iden.delegation_sender_pubkey).hex(), "yellow"))
+        print(colored("Sender delegation signature " \
+                      + bytes(envelop["sender_delegation"][0]["signature"]).hex(), "yellow"))
+        print(colored("Sender delegation signature, decoded cbor: ", "red"),
+                      cbor2.loads(bytes(envelop["sender_delegation"][0]["signature"])))
+        print(colored("Sender delegation delegation pubkey " \
+                      + bytes(envelop["sender_delegation"][0]["delegation"]["pubkey"]).hex(), "yellow"))
+        print(colored("sender_sig " \
+                      + bytes(sig[1]).hex(), "yellow"))
+        print(colored("sender_pubkey " \
+                      + bytes(sig[0]).hex(), "yellow"))
 
     print(colored(envelop, "yellow"))
+    print("cbor encoding", colored("evenlop", "red"), envelop)
+    
     c = cbor2.dumps(envelop)
-    print("cbord encoded", c.hex())
+    print("cbord encoded", colored("evenlop", "red"), c.hex())
     return req_id, c
 
 # According to did, get the method returned param type
@@ -67,7 +84,27 @@ class Agent:
 
     def query_endpoint(self, canister_id, data):
         ret = self.client.query(canister_id, data)
-        print(ret)
+        print("query_endpoint ret", ret)
+        try:
+            ret_str = ret.decode('utf-8')
+            if 'Failed to authenticate' in ret_str:
+                print('Failed to authenticate .. analysing string', ret_str)
+
+            m = re.search("signature d9d9[a-z0-9]*", ret_str)
+            if m:
+                cbor = m[0][len("signature")+1:]
+                cbor_decoded = cbor2.loads(bytes.fromhex(cbor))
+                print("CBOR ", cbor_decoded)
+                print(bytes(cbor_decoded["certificate"]).hex())
+
+                # Certificate is naother CBOR encoded value
+                inner_cbor = cbor2.loads(bytes(cbor_decoded["certificate"]))
+                print("tree", inner_cbor["tree"][1])
+                print("signature", bytes(inner_cbor["signature"]).hex())
+                print(inner_cbor.keys())
+                
+        except (UnicodeDecodeError, AttributeError):
+            pass
         return cbor2.loads(ret)
 
     def call_endpoint(self, canister_id, request_id, data):
@@ -90,7 +127,10 @@ class Agent:
         }
         _, data = sign_request(req, self.identity)
         result = self.query_endpoint(canister_id, data)
-        print(result)
+        print("result in query_raw", result)
+        # result in query_raw {'status': 'replied', 'reply': {'arg': b'DIDL\        
+        if isinstance(result, dict) and "reply" in result:
+            print('candid encoded result, which we decode later', result["reply"]["arg"])
         if result['status'] == 'replied':
             if len(arg) == 1:
                 res = decode(result['reply']['arg'])
@@ -111,8 +151,8 @@ class Agent:
             'ingress_expiry': self.get_expiry_date()
         }
         req_id, data = sign_request(req, self.identity)
-        _ = self.call_endpoint(canister_id, req_id, data)
-        # print('update.req_id:', req_id.hex())
+        req = self.call_endpoint(canister_id, req_id, data)
+        print(colored('update_raw - req', 'blue'), req)
         status, result = self.poll(canister_id, req_id)
         if status != 'replied':
             return  status
@@ -149,7 +189,7 @@ class Agent:
         else:
             return status.decode(), cert
 
-    def poll(self, canister_id, req_id, delay=1, timeout=10):
+    def poll(self, canister_id, req_id, delay=1, timeout=30):
         status = None
         for _ in wait(delay, timeout):
             status, cert = self.request_status_raw(canister_id, req_id)
